95f113256d0bb097bc0b06e0fd6ac3ce1fe236db
gitGood6969 <72687984+gitGood6969@users.noreply.github.com>
2021-08-26 23:19:29 +0800

    Added FileServer JS

---
+++ 13fileServer.js
@@ -0,0 +1,55 @@
+var http = require("http");
+var url = require("url");
+var fs = require("fs");
+
+http.createServer(function (urlRequestFromUser, urlResponseToBeSentToUser) {
+
+	var urlAddressAsString = getUrlAddressFrom(urlRequestFromUser);
+	var urlObject = convertUrlStringToUrlObject(urlAddressAsString);
+	var nameOfFileToBeOpened = getFileNameFrom(urlObject);
+
+	readFileAndDisplayWebpage(nameOfFileToBeOpened, urlResponseToBeSentToUser);
+	
+}).listen(8080);
+
+function getUrlAddressFrom(urlRequestedbyUser) {
+	return urlRequestedbyUser.url;
+}
+
+function convertUrlStringToUrlObject(urlAddressAsString) {
+	return url.parse(urlAddressAsString, true); 
+}
+
+function getFileNameFrom(urlObject) {
+	return "." + urlObject.pathname;
+}
+
+function readFileAndDisplayWebpage(nameOfFileToBeOpened, urlResponseToBeSentToUser) {
+	fs.readFile(nameOfFileToBeOpened, function(err, data) {
+		displayWebpage(err, data, urlResponseToBeSentToUser);
+	});
+}
+
+function displayWebpage(err, data, urlResponseToBeSentToUser) {
+	if (thereIsNoErrorMessageAsPageExists(err)) {
+		displayRequestedWebpage(data, urlResponseToBeSentToUser);
+	} else {
+		displayErrorWebpage(urlResponseToBeSentToUser);		
+	}
+}
+
+function thereIsNoErrorMessageAsPageExists(err) {
+	return !err;
+}
+
+function displayRequestedWebpage(data, urlResponseToBeSentToUser) {
+	urlResponseToBeSentToUser.writeHead(200, {"Content-Type": "text/html"});
+	urlResponseToBeSentToUser.write(data);
+	return urlResponseToBeSentToUser.end();
+}
+
+function displayErrorWebpage(urlResponseToBeSentToUser) {
+	urlResponseToBeSentToUser.writeHead(404, {"Content-Type": "text/html"});
+	return urlResponseToBeSentToUser.end("404 Not Found");
+}
+
